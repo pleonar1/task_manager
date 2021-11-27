cc8eb2d4a35e7153ea784c237cb1b2bc85411054
Paul Leonard <paulpaulleonard@gmail.com>
2021-11-27 14:48:25 -0500

    Forgot to be committing

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/tasks_controller.rb
+
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    require "pry"; binding.pry
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    require "pry"; binding.pry
+    task.save
+    require "pry"; binding.pry
+    redirect_to '/tasks'
+  end
+end
